3d6c721319d539e0d449774036fcf282574f4957
brandonTsai <brandonya125@gmail.com>
2019-05-30 15:53:11 -0700

    update for security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here to connect to Clarifai.
+const app = new Clarifai.App({
+  apiKey: '32e6553e88c94bfb8d25f62d826fd97e'
+ });
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -9,5 +25,6 @@ const handleImage = (req, res, db) => {
   .catch(err => res.status(400).json('Unable to get entries!'))
 }
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 }
